beda266970e4a9074861d68cd02c667dcda3702d
Jeese <jeesewu@gmail.com>
2021-05-11 00:36:14 +0800

    Dropdown Widget

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   {
@@ -18,10 +19,39 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
+  const handleToogleButtonClick = (event) => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <div>
-      <Search />
+      <button onClickCapture={handleToogleButtonClick}>Toggle Dropdown</button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
